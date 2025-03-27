eda27e7832321591e5026c807f431a2c888e25d3
Minus <pw07vck@eduvaud.ch>
2025-03-27 20:54:25 +0100

    chore: update FastAPI version in main application

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
 from app.routes import main as router
 
-app = FastAPI(title="Load", version="0.0.1")
+app = FastAPI(title="Load", version="1.1.0")
 app.include_router(router.api_router)
